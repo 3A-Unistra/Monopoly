2cd73e4a4e298837215d2dcf14e90e493a8766a1
Christophe Pierson <spaceapplez@gmail.com>
2022-02-01 19:06:24 +0000

    Replace MovementUnity.cs

--- MovementUnity.cs
+++ MovementUnity.cs
@@ -1,33 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BoardSocket
-{
-    public int x;
-    public int y;
-    public BoardSocket(int x, int y)
-    {
-        this.x=x;
-        this.y=y;
-    }
-}
-
-public class PawnMovement : MonoBehaviour 
-{
-    public float Movespeed;
+namespace Monopoly.MovementTest
+{    
+ public class MovementUnity : MonoBehaviour 
+ {
+    public float Movespeed=2f;
     void Start ()
     {
-        RG2D=GetComponent<Rigidbody2D>();
-        BoardSocket b1=new BoardSocket(0,0);
-        BoardSocket b2=new BoardSocket(4,0);
-        Movespeed=10f;
     }
     void Update()
     {
-        if(RG2D.x != b2.x)
+        if(transform.position.z<8.0)
         {
-            RG2D.velocity=new Vector(Movespeed,0)
+            transform.Translate(Vector3.forward*Time.deltaTime*Movespeed);
         }
     }
+ }
 }
