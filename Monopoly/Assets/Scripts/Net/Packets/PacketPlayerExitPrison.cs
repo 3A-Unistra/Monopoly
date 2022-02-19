930ac3d2f9adeffc0e22a75cfe7d995495df9fc5
Finn Rayment <finn@rayment.fr>
2022-02-19 19:16:28 +0100

    Added skeleton classes for all of the networking packets.
    All network packets as designated in Issues #3 and #4 have been implemented as
    empty classes. They all derive from an abstract parent class Packet which
    handles serialisation and deserialisation of packets. Packet serialisation works
    great and handles itself out of the box. Deserialisation on the other hand is
    going to need some work to parse and cast the object into the concrete
    class-types.

---
+++ Monopoly/Assets/Scripts/Net/Packets/PacketPlayerExitPrison.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using Monopoly.Net;
+
+namespace Monopoly.Net.Packets
+{
+
+    public class PacketPlayerExitPrison : Packet
+    {
+
+        public PacketPlayerExitPrison() : base("PlayerExitPrison")
+        {
+
+        }
+
+    }
+
+}
