a886b581f08989efec7e045f77ddb3e6814951ed
Christophe Pierson <spaceapplez@gmail.com>
2022-02-22 12:35:04 +0000

    Update Monopoly.Player.cs

--- Monopoly.Player.cs
+++ Monopoly.Player.cs
@@ -204,7 +204,7 @@ namespace Monopoly.Player
     }
     bool buyHotel()
     {
-        if( NbHotel > 0)
+        if(NbHotel > 0)
         {
             NbHotel -= 1;
             return true
@@ -221,17 +221,17 @@ namespace Monopoly.Player
     }
     void buyProperty(Player p, Ownable s)
     {
-        if(p.Money > s.price )
+        if(p.Money > s.price)
         {
             s.setowner(p);
-            p.Money -=s.price;
+            p.Money -= s.price;
         }
     }
     void sellProperty(Player p, Ownable s)
     {
 
         s.setowner(NULL);
-        p.Money +=s.price/2;
+        p.Money += s.price/2;
 
     }
  }
@@ -250,7 +250,7 @@ namespace Monopoly.Player
     public List<Square> squareOwned(Player p)
     {
         List<square> tempList = new List<square>()
-        for( int i=0; i<40; i++)
+        for(int i = 0;i < 40;i++)
         {
             if(board[i].owner == p)
                 tempList.Add(board[i]);
