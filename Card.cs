6f38d26dc538274ae764bf4f256b6d126af35d05
Christophe Pierson <spaceapplez@gmail.com>
2022-02-22 12:53:36 +0000

    Update monopoly.Card.cs

---
+++ Card.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Monopoly.Classes
+{
+    public class Card
+    {
+        public string type;
+        public int id;
+        public string desc;
+        public Card(string type,int id,string desc)
+        {
+            this.id = id;
+            this.type = type;
+            this.desc = desc;
+        }
+    }
+}
