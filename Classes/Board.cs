69bb9bdb3c4d246aa8bc83e188a315e617b72dc0
Christophe Pierson <spaceapplez@gmail.com>
2022-02-22 13:37:02 +0000

    Update Board.cs

--- Classes/Board.cs
+++ Classes/Board.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 
 namespace Monopoly.Classes
 {
-
     public class Board
     {
         private Square board[];
         private List<Card> deckCommunity;
         private List<Card> deckChance;
         private int prisonSquare;
-        private int boardMoney;
-        public Square getSquare(int pos)
+        public int BoardMoney
+        {
+            get;
+            set;
+        }
+        public Square GetSquare(int pos)
         {
             return board[pos];
         }
-        public List<Square> squareOwned(Player p)
+        public List<Square> SquareOwned(Player p)
         {  
             List<square> tempList = new List<square>()
             for(int i = 0;i < 40;i++)
@@ -27,9 +30,32 @@ namespace Monopoly.Classes
             }
             return tempList;
         }
-        public static List<property> getPropertySet(color c)
+        public static List<property> GetPropertySet(color c)
         {
-
+            static List<property> propertySet = new List<property>();
+            for(int i = 0; i < 40; i++)
+            {
+                if(Board[i].color == c)
+                    propertySet.add(Board[i]);
+            }
+            return propertySet;
+        }
+        public void FreeParking(Player p)
+        {
+            p.Money += BoardMoney;
+            BoardMoney = 0;
+        }
+        public void AddMoney(Player p; int i)
+        {
+            p.Money += i;
+        }
+        Card GetRandomChanceCard()
+        {
+            return deckChance[rnd.Next(1,deckChance.Count)]
+        }
+        Card GetRandomCommunityCard()
+        {
+            return deckCommunity[rnd.Next(1,deckCommunity.Count)]
         }
     }
 }
