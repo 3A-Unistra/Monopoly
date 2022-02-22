ff218e1919a7c4e46368eab0ff27d73e4066cf02
Christophe Pierson <spaceapplez@gmail.com>
2022-02-22 12:45:27 +0000

    Update Monopoly.Board.cs

--- Monopoly.Board.cs
+++ Monopoly.Board.cs
@@ -1,27 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class Board
- {
-    private Square board[];
-    private List<Card> deckCommunity;
-    private List<Card> deckChance;
-    private int prisonSquare;
-    private int boardMoney;
-    public Square getSquare(int pos)
+namespace Monopoly.Player
+{
+
+    public class Board
     {
-        return board[pos];
-    }
-    public List<Square> squareOwned(Player p)
-    {
-        List<square> tempList = new List<square>()
-        for(int i = 0;i < 40;i++)
+        private Square board[];
+        private List<Card> deckCommunity;
+        private List<Card> deckChance;
+        private int prisonSquare;
+        private int boardMoney;
+        public Square getSquare(int pos)
         {
-            if(board[i].owner == p)
-                tempList.Add(board[i]);
+            return board[pos];
         }
-        return tempList;
-    }
-    public static List<property> getPropertySet(color c)
-    {
+        public List<Square> squareOwned(Player p)
+        {  
+            List<square> tempList = new List<square>()
+            for(int i = 0;i < 40;i++)
+            {
+                if(board[i].owner == p)
+                    tempList.Add(board[i]);
+            }
+            return tempList;
+        }
+        public static List<property> getPropertySet(color c)
+        {
 
+        }
     }
 }
