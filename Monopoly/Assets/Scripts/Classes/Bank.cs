c61eab7c7b58bbd40948eac583e2fdf2cc70cbf1
Christophe Pierson <spaceapplez@gmail.com>
2022-02-24 21:06:23 +0000

    Update Bank.cs

--- Monopoly/Assets/Scripts/Classes/Bank.cs
+++ Monopoly/Assets/Scripts/Classes/Bank.cs
@@ -59,6 +59,14 @@ namespace Monopoly.Classes
             }
             return false;
         }
+        /**
+        * <summary>
+        *   take a hotel from the bank if there are any
+        *  <return>
+        *   true if you took a hotel frm the bank, false if there are none
+        *  </return>
+        * </summary>
+        */
         bool BuyHotel()
         {
             if(NbHotel > 0)
@@ -68,14 +76,34 @@ namespace Monopoly.Classes
             }   
             return false;
         }
+        /**
+        * <summary>
+        *   increment the number of houses by one as the player sells one
+        * </summary>
+        */
         void SellHouse()
         {
             NbHouse++;
         }
+        /**
+        * <summary>
+        *   increment the number of hotels by one as the player sells one
+        * </summary>
+        */        
         void SellHotel()
         {
             NbHotel++;
         }
+        /**
+        * <summary>
+        *   player p buys the property on the square s
+        *   the square s owner is set to p
+        *   the player p's money is decreased by the value of square s
+        * <parameters>
+        *   player p the one who buys the square and 
+        *   Ownablesquare s the square which is bought  
+        * </summary>
+        */          
         void BuyProperty(Player p, OwnableSquare s)
         {
             if(p.Money > s.Price)
@@ -84,6 +112,16 @@ namespace Monopoly.Classes
                 p.Money -= s.Price;
             }
         }
+        /**
+        * <summary>
+        *   player p sells the property on the square s
+        *   the square s owner is set to null
+        *   the player p's money is increased by half the value of square s
+        * <parameters>
+        *   player p the one who sells the square and 
+        *   Ownablesquare s the square which is sold  
+        * </summary>
+        */         
         void SellProperty(Player p, OwnableSquare s)
         {
             s.Owner = null;
