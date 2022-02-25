0d2f0cf5d36ccac27b3d8974187f370afb2ee5cb
Christophe Pierson <spaceapplez@gmail.com>
2022-02-25 10:15:08 +0000

    Update Board.cs

--- Monopoly/Assets/Scripts/Classes/Board.cs
+++ Monopoly/Assets/Scripts/Classes/Board.cs
@@ -1,4 +1,4 @@
-/*
+/**
  * Fichier.cs
  * Fichier définissant la classe plateau et ses 
  * interaction avec les différentes cases
@@ -13,12 +13,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
-  *<summary>
-    *Class Board listing the squares of the board
-    *depending on their type and their set
-    *Also store money for the player to get at free parking
-  *</summary>
+/**
+* <summary>
+* Class Board listing the squares of the board
+* depending on their type and their set
+* Also store money for the player to get at free parking
+* </summary>
 */
 namespace Monopoly.Classes
 {
@@ -39,17 +39,17 @@ namespace Monopoly.Classes
             get;
             set;
         }
-    /*
-    *<summary>
-        *get the square at position pos from the list of elements of the board
-        *<parameter>
-           *int pos the position of the square we want to get
-        *</parameter>
-        *<return>
-            *the square at position pos
-        *</return>
-    *</summary>
-    */
+        /**
+        * <summary>
+        * get the square at position pos from the list of elements of the board
+        * <parameter>
+        * int pos the position of the square we want to get
+        * </parameter>
+        * <return>
+        * the square at position pos
+        * </return>
+        * </summary>
+        */
         public Square GetSquare(int pos)
         {
             return Elements[pos];
@@ -68,17 +68,17 @@ namespace Monopoly.Classes
             }
             return tempList;
         }
-    /*
-    *<summary>
-        *returns the set of properties of color c
-        *<parameter>
-           *Color c the color of the set 
-        *</parameter>
-        *<return>
-            *propertySet the set of properties of color c
-        *</return>
-    *</summary>
-    */
+        /**
+        * <summary>
+        * returns the set of properties of color c
+        * <parameter>
+        * Color c the color of the set 
+        * </parameter>
+        * <return>
+        * propertySet the set of properties of color c
+        * </return>
+        * </summary>
+        */
 
         public static List<PropertySquare> GetPropertySet(Color c)
         {
@@ -94,29 +94,29 @@ namespace Monopoly.Classes
             }
             return propertySet;
         }
-    /*
-    *<summary>
-        *player p gets the money from free parking
-        *resets free parking money
-        *<parameter>
-           *player p the player who landed on free parking
-        *</parameter>
-    *</summary>
-    */
+        /**
+        * <summary>
+        * player p gets the money from free parking
+        * resets free parking money
+        * <parameter>
+        * player p the player who landed on free parking
+        * </parameter>
+        * </summary>
+        */
         public void FreeParking(Player p)
         {
             p.Money += BoardMoney;
             BoardMoney = 0;
         }
-    /*
-    *<summary>
-        *method to add i amount of money to player p money
-        *<parameter>
-           *player p the player who recieves money,
-           * int i the amount of money
-        *</parameter>
-    *</summary>
-    */        
+        /*
+        * <summary>
+        * method to add i amount of money to player p money
+        * <parameter>
+        * player p the player who recieves money,
+        * int i the amount of money
+        * </parameter>
+        * </summary>
+        */        
         public void AddMoney(Player p, int i)
         {
             p.Money += i;
