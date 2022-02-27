644db4a5ceef7342d0c2e2a9d606f6794442b2eb
Finn Rayment <finn@rayment.fr>
2022-02-27 12:26:20 +0100

    Added perspective switching and finalised CameraLook.cs.
    Documented CameraLook.cs, cleaned up the script and added the ability to shift
    perspective between top-down and isometric viewing angles.

--- Monopoly/Assets/Scripts/Camera/CameraLook.cs
+++ Monopoly/Assets/Scripts/Camera/CameraLook.cs
@@ -1,3 +1,11 @@
+/*
+ * CameraLook.cs
+ * Camera rotation and zoom script.
+ * 
+ * Date created : 26/02/2022
+ * Author       : Finn RAYMENT <rayment@etu.unistra.fr>
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,34 +13,101 @@ using UnityEngine;
 namespace Monopoly.Camera
 {
 
+    /**
+     * <summary>
+     *     Camera rotation and zoom script.
+     *     Ensure the camera's pivot point is setup in an isometric starting
+     *     position to ensure the script works correctly.
+     * </summary>
+     */
     [RequireComponent(typeof(UnityEngine.Camera))]
     public class CameraLook : MonoBehaviour
     {
 
+        /**
+         *  <summary>
+         *      Camera viewing mode enum.
+         *  </summary>
+         */
         public enum CameraLookMode
         {
             ISOMETRIC, TOP_DOWN
         }
 
-        [Range(-15.0f, 15.0f)]
-        public float height = 6.0f;
+        /**
+         * <summary>
+         * The Euclidian distance from the camera to the pivot point.
+         * This needs to be large enough to avoid plane-clipping between
+         * the near-plane of the camera and the board, and the far-plane with
+         * the distant side.
+         * </summary>
+         */
         [Range(0.0f, 20.0f)]
         public float distance = 10.0f;
-        [Range(1.0f, 10.0f)]
-        public float zoomMin = 1.0f;
-        [Range(1.0f, 10.0f)]
-        public float zoomMax = 10.0f;
-        public GameObject pivotPoint;
-        [Range(2.0f, 0.01f)]
-        public float rotationSpeed = 1.0f;
-        public float zoomSpeed = 5.0f;
-        public float moveSpeed = 5.0f;
-        public CameraLookMode lookMode = CameraLookMode.ISOMETRIC;
 
         private UnityEngine.Camera cam;
-        private bool pivoting = false;
-        private float pivotTime = 0.0f;
         private Quaternion pivotFrom, pivotTo;
+        /**
+         * <summary>
+         *     The pivot point from which all of the camera rotations and
+         *     movements take place. This point should be flat on the surface of
+         *     the game board.
+         * </summary>
+         */
+        public GameObject pivotPoint;
+        private CameraLookMode lookMode = CameraLookMode.ISOMETRIC;
+
+        /**
+         * <summary>
+         *     The number of seconds that a rotation should take to complete.
+         * </summary>
+         */
+        [Range(0.01f, 2.0f)]
+        public float rotationSpeed = 0.25f;
+        /**
+         * <summary>
+         *     The minimum zoom (closest the board) possible for the camera.
+         * </summary>
+         */
+        [Range(1.0f, 10.0f)]
+        public float zoomMin = 1.0f;
+        /**
+         * <summary>
+         *     The maximum zoom (furthest from the board) possible for the camera.
+         * </summary>
+         */
+        [Range(1.0f, 10.0f)]
+        public float zoomMax = 10.0f;
+        /**
+         * <summary>
+         *     The speed at which zoom should occur for the camera.
+         * </summary>
+         */
+        [Range(0.01f, 20.0f)]
+        public float zoomSpeed = 5.0f;
+        /**
+         * <summary>
+         *     The number of seconds that a switch between isometric and top-down
+         *     viewing angles should take to occur.
+         * </summary>
+         */
+        [Range(0.01f, 2.0f)]
+        public float modeSpeed = 0.25f;
+
+        private bool modeToggle = false, rotateToggle = false;
+        /**
+         * <summary>
+         *     Is the camera currently locked in an animation?
+         * </summary>
+         */
+        [HideInInspector]
+        public bool Animating
+        {
+            get;
+            private set;
+        }
+        private float animateTime = 0.0f;
+        private int rotationSide = 0;
 
         void Start()
         {
@@ -40,9 +115,9 @@ namespace Monopoly.Camera
             cam = GetComponent<UnityEngine.Camera>();
             cam.orthographic = true;
             cam.transform.localPosition = new Vector3(0,
-                                                      height,
+                                                      0,
                                                       -axisDistance * distance);
-            cam.transform.localRotation = Quaternion.Euler(30, 0, 0);
+            cam.transform.localRotation = Quaternion.identity;
             if (pivotPoint == null)
             {
                 Debug.LogError("Camera has no active pivot point.");
@@ -50,64 +125,125 @@ namespace Monopoly.Camera
             }
         }
 
+        /**
+         * <summary>
+         *     Toggle the camera viewing mode between isometric and top-down.
+         * </summary>
+         */
+        private void ToggleCameraMode()
+        {
+            if (!Animating)
+            {
+                Animating = true;
+                modeToggle = true;
+                pivotFrom = pivotPoint.transform.localRotation;
+                if (lookMode == CameraLookMode.TOP_DOWN)
+                {
+                    lookMode = CameraLookMode.ISOMETRIC;
+                    pivotTo = Quaternion.Euler(30, 45+(90*rotationSide), 0);
+                }
+                else
+                {
+                    lookMode = CameraLookMode.TOP_DOWN;
+                    pivotTo = Quaternion.Euler(90, 90+(90*rotationSide), 0);
+                }
+                animateTime = 0.0f;
+            }
+        }
+
+        /**
+         * <summary>
+         *     Rotate the camera a full 90 degrees left or right.
+         * </summary>
+         * <param name="dir">
+         *     The direction in which the rotation should take place.
+         *     This value is clamped between -1 and 1. If the value given is
+         *     equal to 0, then no rotation shall take place.
+         * </param>
+         */
+        private void RotateCamera(int dir)
+        {
+            if (!Animating && dir != 0)
+            {
+                Animating = true;
+                rotateToggle = true;
+                pivotFrom = pivotPoint.transform.localRotation;
+                dir = dir < 0 ? -1 : 1; // -1,1 clamp
+                rotationSide += dir;
+                if (rotationSide >= 4)
+                    rotationSide -= 4;
+                else if (rotationSide <= -4)
+                    rotationSide += 4;
+                pivotTo = Quaternion.Euler(0, dir * 90, 0) * pivotFrom;
+                animateTime = 0.0f;
+            }
+        }
+
+        /**
+         * <summary>
+         *     Zoom the camera in or out by a given delta.
+         * </summary>
+         * <param name="delta">
+         *     The delta to zoom by. This is typically the delta motion of the
+         *     mouse wheel.
+         * </param>
+         */
+        private void ZoomCamera(float delta)
+        {
+            float zoomDisplacement = delta * Time.deltaTime * zoomSpeed;
+            cam.orthographicSize =
+                Mathf.Clamp(cam.orthographicSize + zoomDisplacement,
+                            zoomMin, zoomMax);
+        }
+
+        /**
+         * <summary>
+         *     Animate a rotation or mode switch on the camera.
+         *     This locks out any other rotation request until the animation
+         *     is complete.
+         * </summary>
+         */
+        private void AnimateCamera()
+        {
+            if (modeToggle || rotateToggle)
+            {
+                // animate camera mode toggle
+                animateTime += Time.deltaTime / modeSpeed;
+                if (animateTime >= 1.0f)
+                {
+                    // end of animation, set to destination quaternion
+                    Animating = false;
+                    modeToggle = false;
+                    rotateToggle = false;
+                    pivotPoint.transform.localRotation = pivotTo;
+                }
+                else
+                {
+                    // animating, add to rotation
+                    pivotPoint.transform.localRotation =
+                        Quaternion.Lerp(pivotFrom, pivotTo, animateTime);
+                }
+            }
+        }
+
         void Update()
         {
-            if (lookMode == CameraLookMode.ISOMETRIC)
+            if (Input.GetKeyDown(KeyCode.F))
             {
-                // isometric view code
-                if (!pivoting)
-                {
-                    // camera movement
-                    float horDir = Input.GetAxis("Horizontal");
-                    pivotPoint.transform.localPosition +=
-                        cam.transform.right *
-                        horDir * moveSpeed * Time.deltaTime;
-                    float verDir = Input.GetAxis("Vertical");
-                    pivotPoint.transform.localPosition +=
-                        cam.transform.up *
-                        verDir * moveSpeed * Time.deltaTime;
-                    // camera rotatation
-                    int pivotDir = (Input.GetKeyDown(KeyCode.Q) ? 1 : 0) +
-                                   (Input.GetKeyDown(KeyCode.E) ? -1 : 0);
-                    if (pivotDir != 0)
-                    {
-                        pivotFrom = pivotPoint.transform.localRotation;
-                        pivotTo =
-                            pivotFrom * Quaternion.Euler(0, pivotDir * 90, 0);
-                        pivotTime = 0.0f;
-                        pivoting = true;
-                    }
-                }
-                else if (pivoting)
-                {
-                    pivotTime += Time.deltaTime / rotationSpeed;
-                    if (pivotTime < 1.0f)
-                    {
-                        pivotPoint.transform.localRotation =
-                            Quaternion.Lerp(pivotFrom, pivotTo, pivotTime);
-                    }
-                    else
-                    {
-                        pivoting = false;
-                        pivotPoint.transform.localRotation = pivotTo;
-                    }
-                }
-                // mouse zoom
-                if (Input.mouseScrollDelta.y != 0)
-                {
-                    float zoomDisplacement =
-                        Input.mouseScrollDelta.y * Time.deltaTime * zoomSpeed;
-                    Debug.Log(Input.mouseScrollDelta.y);
-                    cam.orthographicSize =
-                        Mathf.Clamp(cam.orthographicSize + zoomDisplacement,
-                                    zoomMin, zoomMax);
-                }
+                ToggleCameraMode();
             }
-            else
+            else if (Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.E))
             {
-                // top down view code
-
+                int pivotDir = (Input.GetKeyDown(KeyCode.Q) ? 1 : 0) +
+                               (Input.GetKeyDown(KeyCode.E) ? -1 : 0);
+                RotateCamera(pivotDir);
             }
+            if (Input.mouseScrollDelta.y != 0)
+            {
+                ZoomCamera(Input.mouseScrollDelta.y);
+            }
+            if (Animating)
+                AnimateCamera();
         }
 
     }
