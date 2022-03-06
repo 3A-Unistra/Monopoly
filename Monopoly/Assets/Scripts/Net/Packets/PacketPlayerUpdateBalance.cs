dc5583fcca1c5d6ee40c19ee52f6d1a8a78f22d5
Finn Rayment <finn@rayment.fr>
2022-03-06 19:19:08 +0100

    Fixed merge.

--- Monopoly/Assets/Scripts/Net/Packets/PacketPlayerUpdateBalance.cs
+++ Monopoly/Assets/Scripts/Net/Packets/PacketPlayerUpdateBalance.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 using Monopoly.Net;
 
@@ -8,6 +9,27 @@ namespace Monopoly.Net.Packets
 
     public class PacketPlayerUpdateBalance : Packet
     {
+
+        [JsonProperty("id_player")]
+        public string PlayerId
+        {
+            get;
+            private set;
+        }
+
+        [JsonProperty("old_balance")]
+        public int OldBalance
+        {
+            get;
+            private set;
+        }
+
+        [JsonProperty("new_balance")]
+        public int NewBalance
+        {
+            get;
+            private set;
+        }
 
         public PacketPlayerUpdateBalance() : base("PlayerUpdateBalance")
         {
