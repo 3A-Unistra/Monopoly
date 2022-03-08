33997bd0fceea1d4edb916b829fd29fd0faeb606
Maxime Maire <maxime.maire2@etu.unistra.fr>
2022-03-08 18:08:18 +0000

    Ajout  des Jsonproperty

--- Monopoly/Assets/Scripts/Net/Packets/PacketActionExchangeAccept.cs
+++ Monopoly/Assets/Scripts/Net/Packets/PacketActionExchangeAccept.cs
@@ -1,11 +1,19 @@
-﻿using System.Collections;
+﻿/*
+ * PacketActionExchangeAccept.cs
+ * 
+ * Date created : 01/03/2022
+ * Author       : Maxime MAIRE <maxime.maire2@etu.unistra.fr>
+ *              : Finn RAYMENT <rayment@etu.unistra.fr>
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 
 using Monopoly.Net;
 
 namespace Monopoly.Net.Packets
 {
-
+    
     public class PacketActionExchangeAccept : Packet
     {
 
--- Monopoly/Assets/Scripts/Net/Packets/PacketPlayerMove.cs
+++ Monopoly/Assets/Scripts/Net/Packets/PacketPlayerMove.cs
@@ -1,6 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/*
+ * PacketPlayerMove.cs
+ * 
+ * Date created : 03/03/2022
+ * Author       : Maxime MAIRE <maxime.maire2@etu.unistra.fr>
+ *              : Finn RAYMENT <rayment@etu.unistra.fr>
+ */
 
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 using Monopoly.Net;
 
 namespace Monopoly.Net.Packets
@@ -8,6 +16,11 @@ namespace Monopoly.Net.Packets
 
     public class PacketPlayerMove : Packet
     {
+        [JsonProperty("id_moving_player")]
+        public string MovingPlayerId { get; private set; }
+
+        [JsonProperty("destination_case")]
+        public int DestinaionSquare { get; private set; }
 
         public PacketPlayerMove() : base("PlayerMove")
         {
