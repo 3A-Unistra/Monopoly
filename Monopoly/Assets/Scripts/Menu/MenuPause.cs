c3384063c7310220c37b809aef8aee32f8cda59d
Rayan Marmar <rayan.marmar@etu.unistra.fr>
2022-03-28 14:41:26 +0000

    Fonctionnalités des Menus

--- Monopoly/Assets/Scripts/Menu/MenuPause.cs
+++ Monopoly/Assets/Scripts/Menu/MenuPause.cs
@@ -1,40 +1,49 @@
+/*
+ * MenuPause.cs
+ * This file contain the event listeners of the pause's Menu buttons.
+ * 
+ * Date created : 28/02/2022
+ * Author       : Rayan MARMAR <rayan.marmar@etu.unistra.fr>
+ *                Maxime MAIRE <maxime.maire2@etu.unistra.fr
+ */
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MenuPause : MonoBehaviour
+namespace Monopoly.Menu
 {
-    public GameObject Menu;
-    public GameObject MenuButton;
-
-    void Start()
+    public class MenuPause : MonoBehaviour
     {
-    }
+        public GameObject PauseMenu;
+        public GameObject MenuOptions;
+        public GameObject PauseButton;
+        public GameObject OptionsButton;
 
-    void Update()
-    {
-    }
-
-    public void MenuDisable()
-    {
-        Menu.SetActive(false);
-        MenuButton.SetActive(true);
-        // il faut aussi activer le bouton pour ouvrir le menu
-    }
-
-    public void OptionActive()
-    {
-        ;
-    }
-
-    public void DisconnectToTheGame()
-    {
-        ;
-    }
-
-    public void QuitGame()
-    {
-        ;
+        public void MenuDisable()
+        {
+            PauseMenu.SetActive(false);
+            PauseButton.SetActive(true);
+            OptionsButton.SetActive(true);
+        }
+    
+        public void OptionActive()
+        {
+            MenuOptions.SetActive(true);
+            PauseMenu.SetActive(false);
+        }
+    
+        public void DisconnectToTheGame()
+        {
+            ;
+        }
+    
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
     }
 }
+
